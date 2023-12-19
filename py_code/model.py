8c27eb0d92e0880e9697a5bd0190e49c769507d7
YoAB25 <youssef.learn.ab.25@gmail.com>
2023-12-19 09:46:43 +0100

    Starter code provided - model and leg of duck

---
+++ py_code/model.py
@@ -0,0 +1,231 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import math
+from numpy import linalg as LA
+
+# Model class fot the bird leg
+class model(object):
+	# constructor
+	def __init__(self, points, angles_hip_joints, angles_ankle_joints):
+		self.M = [[0 for j in range(4)] for i in range(8)]
+		self.a = [0 for j in range(6)]
+		self.alpha = [0 for j in range(6)] 
+		self.d = [0 for j in range(6)] 
+		self.theta = [0 for j in range(6)] 	
+		self.pim =  [0 for j in range(6)] 	
+		self.mip =  [0 for j in range(6)] 	
+		self.psi_h = 0.0
+		self.eta_h = 0.0
+		self.psi_a = 0.0
+		self.eta_a = 0.0
+		# z-axes for rotations
+		self.z0 = np.array([0,0,1]).T
+		## vertical hip yaw
+		self.z1 = self.z0
+		## horiz hip roll
+		self.z2 = np.array([1,0,0]).T
+		## hip inclined joint
+		self.z3 = np.array([0,1,0]).T
+		## knee joint
+		self.z4 = np.array([0,1,0]).T
+		## ankle joint
+		self.z5 = np.array([0,1,0]).T
+		## foot joint
+		self.z6 = np.array([0,1,0]).T
+		## toe joint
+		self.z7 = self.z6
+
+		self.xA = np.array([0,0,0]).T
+		self.yA = np.array([0,0,0]).T
+		self.zA = np.array([0,0,0]).T
+		
+		self.updateDHKKWithStancePosition(points[0],points[1],points[2],points[3],points[4])
+		self.updateDHKKWithOrientationAnglesHipAnkle(angles_hip_joints,angles_ankle_joints)	
+
+
+	#################################################################
+	# updateDHKKWithOrientationAnglesHipAnkle
+	# inputs : orientation angles for the 2nd inclined hip joint and for the ankle joint
+	# outputs : nothing (calculates DHKK parameters for storage inside class instance
+	#################################################################	
+	def updateDHKKWithOrientationAnglesHipAnkle(self,ang_hip,ang_ankle):
+		self.psi_h = ang_hip[0] * math.pi / 180.0
+		self.eta_h = ang_hip[1] * math.pi / 180.0
+		self.psi_a = ang_ankle[0] * math.pi / 180.0
+		self.eta_a = ang_ankle[1] * math.pi / 180.0	
+			## angles of inclined hip joint, psi is horiz, eta is vertical
+		self.z3 = 	np.array([math.sin(self.eta_h)*math.cos(self.psi_h),math.sin(self.eta_h)*math.sin(self.psi_h),math.cos(self.eta_h)]).T
+			# ankle joint axis
+		self.z5 = (math.sin(self.eta_a)*math.cos(self.psi_a))*self.xA + (math.sin(self.eta_a)*math.sin(self.psi_a))*self.yA + 	(math.cos(self.eta_a))*self.zA
+		self.calcDHKK()
+			
+		#################################################################
+		# updateDHKKWithStancePosition
+		# inputs : 3D coord of hip, knee, angle and foot points
+		# outputs : nothing (calculates DHKK parameters for storage inside class instance
+		#################################################################	
+	def updateDHKKWithStancePosition(self,H,K,A,F,T):	
+			self.M[0] = H - H
+			self.M[1] = self.M[0]
+			self.M[2] = self.M[0]
+			self.M[3] = self.M[0]
+			self.M[4] = K - H	
+			self.M[5] = A - H
+			self.M[6] = F - H
+			self.M[7] = T - H         
+			KA = A - K
+			HK = K - H
+			self.xA = KA/LA.norm(KA)
+			self.yA = np.cross(self.xA,HK)/ LA.norm(np.cross(self.xA,HK))
+			self.zA = np.cross(self.xA,self.yA)
+			# knee joint axis
+			self.z4 = -self.yA
+			# ankle joint axis
+			self.z5 = (math.sin(self.eta_a)*math.cos(self.psi_a))*self.xA + (math.sin(self.eta_a)*math.sin(self.psi_a))*self.yA + (math.cos(self.eta_a))*self.zA
+			# foot joint axis
+			FT = T - F
+			self.z6 = np.cross(self.z0,FT)/LA.norm(np.cross(self.z0,FT))
+			# dummy
+			self.z7 = self.z6 
+			# call calc of DHKKS
+			self.calcDHKK()
+		
+		#################################################################
+		# calcDHKK
+		# inputs : none
+		# outputs : none
+		# intern calculation of DHKK parameters
+		#################################################################	
+	def calcDHKK(self):
+			z = np.array([self.z0,self.z1,self.z2,self.z3,self.z4,self.z5,self.z6,self.z7])
+
+			x = [[0 for j in range(3)] for i in range(8)]
+			for i in range(1,7):
+				if LA.norm(np.cross(z[i],z[i+1])) < 0.001 :
+					x[i] = np.cross(np.cross(z[i],self.M[i+1]-self.M[i]),z[i]) / LA.norm(np.cross(z[i],self.M[i+1]-self.M[i])) 
+				else :
+					if ( np.dot(np.cross(z[i],z[i+1]),self.M[i+1]-self.M[i]) < 0 ) :
+						s = -1
+					else :
+						s = 1
+					x[i] = 	s * np.cross(z[i],z[i+1]) / LA.norm(np.cross(z[i],z[i+1]))
+			
+			x[0] = [1,0,0]
+			for i in range(1,7):
+				self.a[i-1] = np.dot(self.M[i]-self.M[i-1],x[i-1])
+				self.alpha[i-1] = math.atan2( np.dot(x[i-1],np.cross(z[i-1],z[i])), np.dot(z[i-1],z[i]))
+				self.theta[i-1] = math.atan2( np.dot(z[i],np.cross(x[i-1],x[i])), np.dot(x[i-1],x[i]))
+			
+				if ( np.dot(np.cross(z[i-1],z[i]),self.M[i]-self.M[i-1]) < 0 ) :
+					s1 = -1
+				else :
+					s1 = 1
+				
+				if ( np.dot(np.cross(z[i],z[i+1]),self.M[i+1]-self.M[i]) < 0 ) :
+					s2 = -1
+				else :
+					s2 = 1
+				
+				if (LA.norm(np.cross(z[i-1],z[i]))) < 0.001:
+					self.pim[i-1] = np.dot(self.M[i]-self.M[i-1],z[i])
+				else :
+					self.pim[i-1] = s1 * np.dot(np.cross(x[i-1],z[i-1]),self.M[i]-self.M[i-1]) / LA.norm(np.cross(z[i-1],z[i])) 
+					
+				if (LA.norm(np.cross(z[i],z[i+1]))) < 0.001:
+					self.mip[i-1] = 0
+				else:		
+					self.mip[i-1] = -s2* np.dot(np.cross(x[i],z[i+1]),self.M[i+1]-self.M[i]) / LA.norm(np.cross(z[i],z[i+1])) 
+					
+				self.d[i-1] = self.pim[i-1] + self.mip[i-1]
+				
+		#################################################################
+		# calc_KneeAnkleFootCoord
+		# inputs: angle commands hip roll, hip incl , knee, ankle
+		# outputs : 3D coordinates of knee, ankle and foot in hip coord. frame
+		#################################################################
+	def calc_KneeAnkleFootCoord(self,qHipRoll=0,qHipIncl=0,qKnee=0,qAnkle=0):
+			# calculate matrices
+			num_matrices = 6
+			ncol = 4
+			nrow = 4
+			T = [[[0 for j in range(ncol)] for i in range(nrow)] for k in range(num_matrices)]
+			q = [0,qHipRoll,qHipIncl,qKnee,qAnkle,0]
+			q_cmd = [0 for j in range(6)] 
+			for i in range(num_matrices):
+				q_cmd[i] =  self.theta[i] + q[i]
+				T[i] = np.array( [[math.cos(q_cmd[i]),-math.sin(q_cmd[i]),0,self.a[i]],[math.cos(self.alpha[i])*math.sin(q_cmd[i]), math.cos(self.alpha[i])*math.cos(q_cmd[i]),-math.sin(self.alpha[i]),-self.d[i]*math.sin(self.alpha[i])],[math.sin(self.alpha[i])*math.sin(q_cmd[i]), math.sin(self.alpha[i])*math.cos(q_cmd[i]),		 math.cos(self.alpha[i]), self.d[i]*math.cos(self.alpha[i])], [0,0,0,1]])
+			
+			TH = np.dot(np.dot(T[0],T[1]),T[2])
+			pt = np.array([[0,0,-self.mip[3],1]]).T
+			TK = np.dot(TH,T[3])
+			K_Rhip = np.dot(TK,pt)
+
+			pt = np.array([[0,0,-self.mip[4],1]]).T
+			TA = np.dot(TK,T[4])
+			A_Rhip = np.dot(TA,pt)
+
+			pt = np.array([[0,0,-self.mip[5],1]]).T
+			TF = np.dot(TA,T[5])
+			F_Rhip = np.dot(TF,pt)
+		
+			return K_Rhip, A_Rhip, F_Rhip
+
+
+		#################################################################
+		# calc_JacobianKneeAnkleFoot
+		# inouts : current angles hip roll, hip incl , knee, ankle
+		# outputs : Jacobian matrices for knee, ankle and foot
+		#################################################################	
+	def calc_JacobianKneeAnkleFoot(self,qHipRoll_prev=0,qHipIncl_prev=9,qKnee_prev=9,qAnkle_prev=0):
+			#print('inside calc jac')
+			num_matrices = 6
+			ncol = 4
+			nrow = 4
+			qcurrent = [0,qHipRoll_prev,qHipIncl_prev,qKnee_prev,qAnkle_prev,0]
+			#print('after qcurrent')
+			q_cmd = [0 for j in range(6)] 
+			DT = [[[0 for j in range(ncol)] for i in range(nrow)] for k in range(num_matrices)]
+			T = [[[0 for j in range(ncol)] for i in range(nrow)] for k in range(num_matrices)]
+			for i in range(num_matrices):
+				q_cmd[i] =  self.theta[i] + qcurrent[i]
+				DT[i] = np.array( [[-math.sin(q_cmd[i]),-math.cos(q_cmd[i]),0,0],[math.cos(self.alpha[i])*math.cos(q_cmd[i]), -math.cos(self.alpha[i])*math.sin(q_cmd[i]),0,0],[math.sin(self.alpha[i])*math.cos(q_cmd[i]), -math.sin(self.alpha[i])*math.sin(q_cmd[i]),0,0], [0,0,0,0]])
+				T[i] = np.array( [[math.cos(q_cmd[i]),-math.sin(q_cmd[i]),0,self.a[i]],[math.cos(self.alpha[i])*math.sin(q_cmd[i]), math.cos(self.alpha[i])*math.cos(q_cmd[i]),-math.sin(self.alpha[i]),-self.d[i]*math.sin(self.alpha[i])],[math.sin(self.alpha[i])*math.sin(q_cmd[i]), math.sin(self.alpha[i])*math.cos(q_cmd[i]),		 math.cos(self.alpha[i]), self.d[i]*math.cos(self.alpha[i])], [0,0,0,1]])
+
+			#print('before pt')
+			pt = np.array([0,0,-self.mip[3],1])
+			MJq1 = np.dot(np.dot(np.dot(T[0],DT[1]),T[2]),T[3])
+			MJq2 = np.dot(np.dot(np.dot(T[0],T[1]),DT[2]),T[3])
+			JKnee_q1 =  np.dot(MJq1,pt)
+			JKnee_q2 =  np.dot(MJq2,pt)
+			JKnee = np.array([JKnee_q1[0:3],JKnee_q2[0:3]]).T
+			#print(JKnee)
+
+			pt = np.array([0,0,-self.mip[4],1])
+			MAq1 = np.dot(MJq1,T[4])
+			MAq2 = np.dot(MJq2,T[4])
+			AInter = np.dot(np.dot(np.dot(T[0],T[1]),T[2]),DT[3])
+			MAq3 = np.dot(AInter,T[4])
+			JAnkle_q1 =  np.dot(MAq1,pt)
+			JAnkle_q2 =  np.dot(MAq2,pt)
+			JAnkle_q3 =  np.dot(MAq3,pt)
+			JAnkle = np.array([JAnkle_q1[0:3],JAnkle_q2[0:3],JAnkle_q3[0:3]]).T
+			#print(JAnkle)
+			
+			pt = np.array([0,0,-self.mip[5],1])
+			MFq1 = np.dot(MAq1,T[5])
+			MFq2 = np.dot(MAq2,T[5])
+			MFq3 = np.dot(MAq3,T[5])
+			Inter1 = np.dot(np.dot(np.dot(T[0],T[1]),T[2]),T[3])
+			FInter = np.dot(Inter1,DT[4])
+			MFq4 = np.dot(FInter,T[5])
+			JFoot_q1 =  np.dot(MFq1,pt)
+			JFoot_q2 =  np.dot(MFq2,pt)
+			JFoot_q3 =  np.dot(MFq3,pt)
+			JFoot_q4 =  np.dot(MFq4,pt)
+			JFoot = np.array([JFoot_q1[0:3],JFoot_q2[0:3],JFoot_q3[0:3],JFoot_q4[0:3]]).T
+			#print(JFoot)
+			
+			return JKnee, JAnkle, JFoot
---
+++ py_code/myleg.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python
+
+#############################################################################
+# imports
+#############################################################################
+import numpy as np
+import math
+import openpyxl
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from numpy import linalg as LA
+
+from model import model
+
+
+#############################################################################
+# main function
+#############################################################################
+if __name__ == '__main__':
+	
+		######################################################################		
+		# read file
+		# Define variable to load the dataframe
+		dataframe = openpyxl.load_workbook("./fich-from-data-scilab/joints_xyz_average_in_VCp_ref_frame_q12.xlsx")
+		#dataframe = openpyxl.load_workbook("./fich-from-data-scilab/joints_xyz_average_in_VCp_ref_frame_mbv.xlsx")
+	 
+		######################################################################		
+		# Define variable to read sheet of data
+		ws = dataframe.active
+		allCells = np.array([[float(cell.value) for cell in row] for row in ws.iter_rows(2,ws.max_row)])
+		xToe = allCells[:,0]
+		yToe = allCells[:,1]
+		zToe = allCells[:,2]
+		xFoot = allCells[:,3]
+		yFoot = allCells[:,4]
+		zFoot = allCells[:,5]
+		xAnkle = allCells[:,6]
+		yAnkle = allCells[:,7]
+		zAnkle = allCells[:,8]
+		xKnee = allCells[:,9]
+		yKnee = allCells[:,10]
+		zKnee = allCells[:,11]
+		xHip = allCells[:,12]
+		yHip = allCells[:,13]
+		zHip = allCells[:,14]
+
+		######################################################################		
+		# plot
+		fig = plt.figure()
+		ax1 = plt.axes(projection='3d')
+		ax1.axis('equal')
+		ax1.set_xlabel('X Long')
+		ax1.set_ylabel('Y Lateral')
+		ax1.set_zlabel('Z Vertical')
+		ax1.plot3D(xToe-xHip, yToe-yHip, zToe-zHip, 'gray')
+		ax1.plot3D(xFoot-xHip, yFoot-yHip, zFoot-zHip, 'blue')
+		ax1.plot3D(xAnkle-xHip, yAnkle-yHip, zAnkle-zHip, 'green')
+		ax1.plot3D(xKnee-xHip, yKnee-yHip, zKnee-zHip, 'orange')
+		ax1.plot3D(xHip-xHip, yHip-yHip, zHip-zHip, 'black')
+		
+		######################################################################		
+		# plot segments at rest position
+		# look for position where projection of knee is in the middle of toe
+		min_ = np.max(xFoot) - np.min(xFoot)
+		diff = math.fabs(xKnee[0] - (xFoot[0]+xToe[0])/2.0)
+		i = 1
+		Nsamples = ws.max_row-1
+		for i in range (Nsamples):
+			if (min_ > diff):
+				min_ = diff
+				k = i
+			diff = math.fabs(xKnee[i] - (xFoot[i]+xToe[i])/2.0)	
+
+		ind_start = k
+		print('ind_start')
+		print(ind_start)
+		seg_x = [0,xKnee[k]-xHip[k],xAnkle[k]-xHip[k],xFoot[k]-xHip[k],xToe[k]-xHip[k]]
+		seg_y = [0,yKnee[k]-yHip[k],yAnkle[k]-yHip[k],yFoot[k]-yHip[k],yToe[k]-yHip[k]]
+		seg_z = [0,zKnee[k]-zHip[k],zAnkle[k]-zHip[k],zFoot[k]-zHip[k],zToe[k]-zHip[k]]
+		ax1.plot3D(seg_x, seg_y, seg_z, 'black')
+		
+		######################################################################			
+		# define arrays for hip, knee, ankle and foot points relative to rest position
+		H = np.array([xHip[ind_start],yHip[ind_start],zHip[ind_start]])
+		K = np.array([xKnee[ind_start],yKnee[ind_start],zKnee[ind_start]])
+		A = np.array([xAnkle[ind_start],yAnkle[ind_start],zAnkle[ind_start]])
+		F = np.array([xFoot[ind_start],yFoot[ind_start],zFoot[ind_start]])
+		T = np.array([xToe[ind_start],yToe[ind_start],zToe[ind_start]])
+		points = np.array([H,K,A,F,T])
+
+		######################################################################		
+		# define orientation angles of inclined hip joint (2nd actuated joint at hip) 
+		# to be optimized
+		ang_hip_incl = np.array([116,55])
+		#ang_hip_incl = np.array([90,90])
+
+		# define orientation angles of inclined ankle joint (4th actuated joint after knee joint 		
+		# to be optimized
+		ang_ankle = np.array([-93,67])
+		#ang_ankle = np.array([90,90])
+				
+		
+		######################################################################		
+		# creat an instance of the leg model using class "model"
+		MyLegLModel = model(points,ang_hip_incl,ang_ankle);
+		
+		# inits of current angles
+		qHipRoll=0
+		qHipIncl=0
+		qKnee=0
+		qAnkle=0
+		# inits of arrays for the angles over the trajectory
+		qH1 = [0 for j in range(Nsamples)]
+		qH2 = [0 for j in range(Nsamples)]
+		qK = [0 for j in range(Nsamples)]
+		qA = [0 for j in range(Nsamples)]
+		dF = [0 for j in range(Nsamples)]
+		Kq = [[0 for j in range(3)] for i in range(Nsamples)]
+		Aq= [[0 for j in range(3)] for i in range(Nsamples)]
+		Fq = [[0 for j in range(3)] for i in range(Nsamples)]
+		
+		# inits of initial points at the beginning of the trajectory
+		# Be careful, start at index chosen for the reference of the model
+		K0 = np.array([xKnee[ind_start]-xHip[ind_start],yKnee[ind_start]-yHip[ind_start],zKnee[ind_start]-zHip[ind_start]])
+		A0 = np.array([xAnkle[ind_start]-xHip[ind_start],yAnkle[ind_start]-yHip[ind_start],zAnkle[ind_start]-zHip[ind_start]])
+		F0 = np.array([xFoot[ind_start]-xHip[ind_start],yFoot[ind_start]-yHip[ind_start],zFoot[ind_start]-zHip[ind_start]])
+		# first variation of foot coordinates
+		deltaFoot = np.array([xFoot[ind_start+1]-xFoot[ind_start],yFoot[ind_start+1]-yFoot[ind_start],zFoot[ind_start+1]-zFoot[ind_start]])
+
+
+		# call calc of direct geometric model
+		K,A,F = MyLegLModel.calc_KneeAnkleFootCoord(qHipRoll,qHipIncl,qKnee,qAnkle)
+		# call cal of Jacobian matrices (knee, ankle and foot)
+		JK,JA,JF = MyLegLModel.calc_JacobianKneeAnkleFoot(qHipRoll,qHipIncl,qKnee,qAnkle)
+		# calc pseudo-inverse for inverse kinematic model
+		Jp = LA.pinv(JF)
+		# calculate variations of angles for joint control
+		Dq = np.dot(Jp,deltaFoot)
+		# calculate Z vector to adjust the commands for joint control (reduce the ampltiude w.r.t. initial joint angles)
+		MaddZ = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]) - np.dot(Jp,JF)
+		Z = np.dot(MaddZ,np.array([qHipRoll,qHipIncl,qKnee,qAnkle]))  
+		# update joint commands with Z
+		nu = 1.0		
+		qHipRoll = Dq[0] - nu * Z[0]
+		qHipIncl = Dq[1] - nu * Z[1]
+		qKnee = Dq[2] - nu * Z[2]
+		qAnkle = Dq[3] - nu * Z[3]		
+	
+		# update arrays of current points, knee, ankle and foot by using the DGM with joint commands
+		KCurrent,ACurrent,FootCurrent = MyLegLModel.calc_KneeAnkleFootCoord(qHipRoll,qHipIncl,qKnee,qAnkle)			
+		Kq[ind_start] = K0.tolist()	
+		Aq[ind_start] = A0.tolist()
+		Fq[ind_start] = F0.tolist()
+		Kq[ind_start+1] = np.squeeze(KCurrent[0:3]).tolist()
+		Aq[ind_start+1] = np.squeeze(ACurrent[0:3]).tolist()
+		Fq[ind_start+1] = np.squeeze(FootCurrent[0:3]).tolist()
+		qH1[ind_start+1] = qHipRoll
+		qH2[ind_start+1] = qHipIncl
+		qK[ind_start+1] = qKnee
+		qA[ind_start+1] = qAnkle
+		
+		# loop for all foot points 3D trajectory (inputs) and get joint angle commands
+		for m in range(2,Nsamples+2):
+			i = (m + ind_start) % Nsamples
+			
+			# update Footnext using np.array
+			Footnext = np.array([xFoot[i]-xHip[i],yFoot[i]-yHip[i],zFoot[i]-zHip[i]])
+			
+			# update deltaFoot using Footnext adn np.array
+			deltaFoot = np.array([Footnext[0]-FootCurrent[0],Footnext[1]-FootCurrent[1],Footnext[2]-FootCurrent[2]])
+			
+			# update Jacobians using model class function
+			JK,JA,JF = MyLegLModel.calc_JacobianKneeAnkleFoot(qHipRoll,qHipIncl,qKnee,qAnkle)			
+			
+			# update pseudo-inverse Jp (J+), use LA library
+			Jp = LA.pinv(JF)					
+			
+			# update angle variations using delatFoot and Jp (use scalar product of numpy, np.dot)
+			Dq = np.dot(Jp,deltaFoot)								
+			
+			# update matrix for additional Z vector, i.e. (I - J+.J), use np.array
+			MaddZ = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]) - np.dot(Jp,JF)
+			
+			# update Z using Z =  [I - J+.J][qHipRoll,qHipIncl,qKnee,qAnkle]^T, use np.dot and np.array
+			Z = np.dot(MaddZ,np.array([qHipRoll,qHipIncl,qKnee,qAnkle]))
+			
+			# update q using Dq and Z : q = q + Dq - nu.Z
+			qHipRoll = qHipRoll + Dq[0] - nu * Z[0]
+			qHipIncl = qHipIncl + Dq[1] - nu * Z[1]
+			qKnee = qKnee + Dq[2] - nu * Z[2]
+			qAnkle = qAnkle + Dq[3] - nu * Z[3]
+			
+			# update current positions of knee, ankle and foot by using the DGM of the model class
+			KCurrent,ACurrent,FootCurrent = MyLegLModel.calc_KneeAnkleFootCoord(qHipRoll,qHipIncl,qKnee,qAnkle)
+			
+			# udpate arrays of joint angles
+			qH1[i] = qHipRoll
+			qH2[i] = qHipIncl
+			qK[i] = qKnee
+			qA[i] = qAnkle
+			
+			Kq[i] = np.squeeze(KCurrent[0:3]).tolist()
+			Aq[i] = np.squeeze(ACurrent[0:3]).tolist()
+			Fq[i] = np.squeeze(FootCurrent[0:3]).tolist()
+			#dF[i] = math.sqrt(deltaFoot[0]**2 + deltaFoot[1]**2 + deltaFoot[2]**2)
+			
+			
+		######################################################################		
+		# add plots of results of kinematic control		
+		######################################################################		
+		ax1.plot3D(np.array(Fq)[:,0],np.array(Fq)[:,1],np.array(Fq)[:,2], mec='red',marker='+')	
+		ax1.plot3D(np.array(Aq)[:,0],np.array(Aq)[:,1],np.array(Aq)[:,2], 'red')					
+		ax1.plot3D(np.array(Kq)[:,0],np.array(Kq)[:,1],np.array(Kq)[:,2], 'red')
+		ax1.legend(['Toe','Foot', 'Ankle','Knee','Hip','Leg segments','Foot command','Ankle command','Knee command'])
+		
+		fig2 = plt.figure()
+		ax2 = plt.axes()
+		ax2.set_xlabel('index')
+		ax2.set_ylabel('q')
+		ax2.plot(180/math.pi*np.array(qH1), 'gray')
+		ax2.plot(180/math.pi*np.array(qH2), 'blue')
+		ax2.plot(180/math.pi*np.array(qK), 'green')
+		ax2.plot(180/math.pi*np.array(qA), 'orange')
+		ax2.legend(['qH1', 'qH2','qK','qA'])
+
+		plt.show()		
+		
+		
+	
